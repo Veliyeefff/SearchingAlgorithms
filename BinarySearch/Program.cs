3a3b0ee5914e35b6a3547cbb2800eb031021eaab
Veliyeefff <6mazgz0@code.edu.az>
2022-11-03 08:37:43 +0400

    Created

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter lenght of array : ");
+            int array_lenght = int.Parse(Console.ReadLine());
+            int[] array = new int[array_lenght];
+            int left = 0;
+            int right = array_lenght-1;
+            int middle;
+            int result = -1;
+
+            Console.WriteLine("Please enter sorted array!!!");
+
+            for (int index = 0; index < array_lenght; index++)
+            {
+                Console.Write($"{index + 1}.Element : ");
+                array[index] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter the number you are looking for : ");
+            int number = int.Parse(Console.ReadLine());
+
+            while (left <= right)
+            {
+                middle = (left + right) / 2;
+
+                if (array[middle] == number)
+                {
+                    Console.WriteLine($"Number was found \nIndex : {middle}");
+                    result = 1;
+                    break;
+                }
+
+                else if(array[middle] > number)
+                {
+                    right = middle - 1;
+                }
+
+                else
+                {
+                    left = middle + 1;                     
+                }
+            }
+            if(result == -1)
+            {
+                Console.WriteLine("Number wasn't found!!!");
+            }
+        }
+    }
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LİnearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter lenght of array : ");
+            int array_lenght = int.Parse(Console.ReadLine());
+            int[] array = new int[array_lenght];
+            int result = -1;
+
+            
+            for(int index = 0; index < array_lenght; index++)
+            {
+                Console.Write($"{index+1}.Element : ");
+                array[index] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter the number you are looking for : ");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int index = 0; index < array_lenght; index++)
+            {
+                if(number == array[index])
+                {
+                    Console.WriteLine($"Number was found... \nIndex : {index}");
+                    result = 1;
+                    break;
+                }
+            }
+            if(result == -1)
+            {
+                Console.WriteLine("Number wasn't found!!!");
+            }
+        }
+    }
+}
